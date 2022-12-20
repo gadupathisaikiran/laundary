cca512508629c3807336717c11022d9fcbffaf55
gadupathisaikiran <saikirangaduparthi1@gmail.com>
2022-12-20 15:20:20 +0530

    frontend code

--- backend/index.js
+++ backend/index.js
@@ -4,9 +4,22 @@ const express=require("express")
 
 const app=express()
 
+const register=require("./router/register")
 
+app.use(register)
 
+const main= async ()=>{
 
+    await mongoose.connect('mongodb+srv://saikiran546:saikiran546@cluster0.bgdbs80.mongodb.net/?retryWrites=true&w=majority')
+  .then(() => console.log('Connected!'))
+
+}
+
+mongoose.set('strictQuery', false)
+main()
+app.listen(3001,()=>{
+    console.log("port is listening")
+})
 
 
 
