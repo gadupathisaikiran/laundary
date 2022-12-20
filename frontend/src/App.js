ba9563d016c88618b8e023f618932300e41aed57
gadupathisaikiran <saikirangaduparthi1@gmail.com>
2022-12-20 15:16:52 +0530

    frontend code

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,26 +2,38 @@ import Createorder from "./createorder";
 import React from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Orderselect from "./orderselect";
+import Summary from "./summary";
+
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 function App() {
+ 
   return (
     <>
-        
-
-       <BrowserRouter>
-         <Routes>
-           <Route path="/selectorder" element={<Orderselect/>} />
 
 
-         </Routes>
-        
-       </BrowserRouter>
-      
-      
-       <Createorder/>
 
-    </>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/selectorder" element={<Orderselect />} />
+          <Route path="/summary" element={<Summary />} />
+          <Route path="/" element={<Createorder/>} />
+
+        </Routes>
+
+      </BrowserRouter>
+
+
+     
+
+
+      </>
+
   );
 }
+
+    
+
+
 
 export default App;
--- frontend/src/createorder.js
+++ frontend/src/createorder.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './createorder.css';
+import "./selectorder.css";
 import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
@@ -35,20 +36,20 @@ const Createorder = () => {
 
                 </div>
 
-             
-                    <Nav id='createbutton'>
 
-                        <Nav.Link href='/selectorder' >Create</Nav.Link>
-
-                    </Nav>
-
-               
-              
+                <Nav id='createbutton'>
+                    <Nav.Link href='/selectorder' >Create</Nav.Link>
 
 
 
+                </Nav>
+</div>
 
 
+
+            <div className='footer'>
+
+                <h5 className='footername'>2021 <img src="https://cdn-icons-png.flaticon.com/512/106/106852.png" alt="img" style={{ height: "20px" }} /> Laundary</h5>
 
             </div>
 
